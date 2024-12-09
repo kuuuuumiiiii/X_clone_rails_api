3ca9be05c89e0cab144b7a7c22fee23655bbbb39
kuuuuumiiiii <kuuuuumiiiii@users.noreply.github.com>
2024-12-10 01:45:56 +0900

    新規登録実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for 'User', at: 'auth'
     end
   end
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  end
 end
